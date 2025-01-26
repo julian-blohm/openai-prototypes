620378c7fe39227f7bf1ed1a8227b85edc3123d7
julian-blohm <julian@startup-werk.de>
2025-01-26 19:21:29 +0100

    add image generator

---
+++ image-generator/image-generator.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+def generate_picture():
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt="fat hamster in comic style doing sky diving",
+        size="1024x1024"
+    )
+    return response.data[0].url
+
+image_url = generate_picture()
+
+print("Generated image URL: ", image_url)
+# get output
